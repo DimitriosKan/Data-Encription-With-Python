2ae05ecd7606554a78b45459979ab1806bee7fea
DimitriosKan <mrhydro7k@gmail.com>
2019-06-15 00:15:24 +0100

    server and client side communication DONE _ cleanup required

--- Client/PyClient.py
+++ Client/PyClient.py
@@ -1,10 +1,21 @@
-from flask import request
+import requests
 from pathlib import Path
 
+# V grabs the public key and makes it into a string V
 home = str(Path.home())
 file = open(home + '/.ssh/id_rsa.pub', 'r')
 public_key = file.read()
 
-data = public_key
-r = request.post('http://127.0.0.1:5000/', data = public_key)
-print(data.text)
+r = requests.post('http://127.0.0.1:5000/', json=public_key) #Data was used previously, json works by default though ...
+# print('Public key response from server:', data)
+# rFromServer = r.json()
+
+dataToSend = {'question':'what is the answer?'} #set up a dictionary to send... equivalent might be %dict(question='what is the answer')%
+res = requests.post('http://127.0.0.1:5000/', json=dataToSend) #post to localhost and jsonify it
+print('response from server:', res.text)
+dataFromServer = res.json() #stores the server's response ? IDK
+
+#
+# data = public_key
+# r = request.post('http://127.0.0.1:5000/', data = public_key)
+# print(data.text)
--- Server/FlaskServer.py
+++ Server/FlaskServer.py
@@ -1,12 +1,28 @@
-from flask import Flask, request
+from flask import Flask, render_template, request, url_for, jsonify
+from pathlib import Path
 
 app = Flask(__name__)
-# @app.route('/', methods = ['POST'])
-@app.route('/')
-def flask_server():
-    x = request.data
-    return "Your post request is: " + str(x)
-    print(x)
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/', methods = ['POST'])
+def flask_server():
+    home = str(Path.home())
+    file = open(home + '/.ssh/id_rsa.pub', 'r')
+    public_key = file.read()
+
+    inbound_data = request.get_json()
+    print('data from client:', inbound_data)
+    return jsonify(public_key)
+
+    # input_json = request.get_json()
+    # print('data from client:', input_json)
+    # dictToReturn = {'here be':'public key'} # have this call the ssh key the way it was done on client side
+    # return jsonify(dictToReturn)
+
+# @app.route('/', methods = ['POST'])
+# def flask_server():
+#     x = request.data
+#     return "Your post request is: " + str(x)
+#     print(x)
+
+if __name__ == '__main__':
+    app.run(debug=True)
