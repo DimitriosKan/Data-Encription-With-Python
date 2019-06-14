c55a070c1e663a6800e43c11c774205b31dd9923
Dimitrios Kanidis <mrhydro7k@gmail.com>
2019-06-14 16:26:15 +0100

    backup

--- Client/PyClient.py
+++ Client/PyClient.py
@@ -1,1 +1,10 @@
-input("Write something here: ")
+from flask import request
+from pathlib import Path
+
+home = str(Path.home())
+file = open(home + '/.ssh/id_rsa.pub', 'r')
+public_key = file.read()
+
+data = public_key
+r = request.post('http://127.0.0.1:5000/', data = public_key)
+print(data.text)
--- Server/FlaskServer.py
+++ Server/FlaskServer.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 
 app = Flask(__name__)
-
-@app.route('/', methods = ['POST'])
-def hello_world():
-    print(request.data)
-    return "You sent me this: " + str(request.data)
+# @app.route('/', methods = ['POST'])
+@app.route('/')
+def flask_server():
+    x = request.data
+    return "Your post request is: " + str(x)
+    print(x)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
